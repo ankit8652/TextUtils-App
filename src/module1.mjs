f73885593072171737b6815d0c779c92a6d2139c
ankitmishra8652 <ankitmishra8652@gmail.com>
2023-12-26 20:53:57 +0530

    props and props types

---
+++ src/module1.mjs
@@ -0,0 +1,6 @@
+let name = "ankit";
+let age = "21";
+let sex = "male";
+
+// export default age;
+export { name, age, sex };
---
+++ src/module2.mjs
@@ -0,0 +1,5 @@
+import { name, age, sex } from "./module1.mjs";
+
+console.log(name);
+console.log(age);
+console.log(sex);
